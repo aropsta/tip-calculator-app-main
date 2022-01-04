901ee4c657e1b3b76486dabbdb0752a535a6475e
aropsta <aropsta16@gmail.com>
2022-01-05 08:32:16 +1100

    css+html finished

--- script.ts
+++ script.ts
@@ -1,1 +1,51 @@
-console.log("Hi");
+const form = document.getElementById("form");
+
+let validate = (e: Object) => {
+  console.log(e);
+};
+
+const buttonClick = (e: Object) => {
+  console.log("Button Clicked!");
+};
+const inputBill: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("bill")
+);
+const inputPeople = document.getElementById("npeople");
+
+const tip = document.getElementById("tipAmount");
+const total = document.getElementById("total");
+
+const reset = document.querySelector(".reset");
+
+form?.addEventListener("submit", validate);
+
+const doStuff = (): void => {};
+
+const addEvent = (obj: (HTMLInputElement | HTMLButtonElement)[]) => {
+  let button: HTMLButtonElement | HTMLInputElement;
+
+  for (button of obj) {
+    button.addEventListener("onclick", buttonClick);
+  }
+};
+
+interface buttonObj {
+  fivePercent: HTMLButtonElement;
+  tenPercent: HTMLButtonElement;
+  fifteenPercent: HTMLButtonElement;
+  twentyFivePercent: HTMLButtonElement;
+  fiftyPercent: HTMLButtonElement;
+  custom: HTMLInputElement;
+}
+
+const buttonPercentages = [
+  <HTMLButtonElement>document.getElementById("percent1"),
+  <HTMLButtonElement>document.getElementById("percent2"),
+  <HTMLButtonElement>document.getElementById("percent3"),
+  <HTMLButtonElement>document.getElementById("percent4"),
+  <HTMLButtonElement>document.getElementById("percent5"),
+  <HTMLInputElement>document.getElementById("percent6"),
+];
+
+addEvent(buttonPercentages);
+buttonClick;
--- script/script.js
+++ script/script.js
@@ -1,2 +1,31 @@
 "use strict";
-console.log("Hi");
+const form = document.getElementById("form");
+let validate = (e) => {
+    console.log(e);
+};
+const buttonClick = (e) => {
+    console.log("Button Clicked!");
+};
+const inputBill = (document.getElementById("bill"));
+const inputPeople = document.getElementById("npeople");
+const tip = document.getElementById("tipAmount");
+const total = document.getElementById("total");
+const reset = document.querySelector(".reset");
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", validate);
+const doStuff = () => { };
+const addEvent = (obj) => {
+    let button;
+    for (button of obj) {
+        button.addEventListener("onclick", buttonClick);
+    }
+};
+const buttonPercentages = [
+    document.getElementById("percent1"),
+    document.getElementById("percent2"),
+    document.getElementById("percent3"),
+    document.getElementById("percent4"),
+    document.getElementById("percent5"),
+    document.getElementById("percent6"),
+];
+addEvent(buttonPercentages);
+buttonClick;
